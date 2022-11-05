bc369d8b508a60b333ff815d1d49ea764ef66613
RobertDoms <robertdoms90@gmail.com>
2022-11-06 00:17:37 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import './css/styles.css';
-
+import { Notify } from 'notiflix';
+import debounce from 'lodash.debounce';
+import { fetchCountries } from './fetchCountries';
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+  input: document.querySelector('#search-box'),
+  countryList: document.querySelector('.country-list'),
+  countryInfo: document.querySelector('.country-info'),
+};
