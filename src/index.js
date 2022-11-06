087ca709b71d52b1abb49105322379e7b629a6a2
RobertDoms <robertdoms90@gmail.com>
2022-11-06 09:36:53 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,3 +9,31 @@ const refs = {
   countryList: document.querySelector('.country-list'),
   countryInfo: document.querySelector('.country-info'),
 };
+
+refs.input.addEventListener('input', debounce(onNameInput, DEBOUNCE_DELAY));
+
+function onNameInput(e) {
+  e.preventDefault();
+  const inputValue = e.target.value.trim();
+  if (!inputValue) {
+    refs.countryList.innerHTML = '';
+    refs.countryInfo.innerHTML = '';
+    return;
+  }
+  fetchCountries(inputValue)
+    .then(data => {
+      console.log(data);
+      if (data.length > 10) {
+        return Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+      } else {
+        renderCountry(data);
+      }
+    })
+    .catch(error => {
+      refs.countryList.innerHTML = '';
+      refs.countryInfo.innerHTML = '';
+      Notify.failure('Oops, there is no country with that name');
+    });
+}
